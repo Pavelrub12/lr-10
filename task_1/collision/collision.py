edcf0dc16a4377d2fe263bf25a77e363c55e71eb
Павел <robi.new1234567@gmail.com>
2025-12-11 12:07:19 +0300

    Task 4 complete

--- task_1/collision/collision.py
+++ task_1/collision/collision.py
@@ -29,3 +29,42 @@ def isCollisionRect(rect1, rect2):
     return True
 
 
+def intersectionAreaRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise RectCorrectError("1й прямоугольник некорректный")
+    
+    if not isCorrectRect(rect2):
+        raise RectCorrectError("1й прямоугольник некорректный")
+    
+    x1_1, y1_1 = rect1[0]
+    x2_1, y2_1 = rect1[1]
+    x1_2, y1_2 = rect2[0]
+    x2_2, y2_2 = rect2[1]
+
+def intersectionAreaRect(rect1, rect2):
+    if not isCorrectRect(rect1):
+        raise ValueError("Некорректный прямоугольник")
+    
+    if not isCorrectRect(rect2):
+        raise ValueError("Некорректный прямоугольник")
+    
+    x1_1, y1_1 = rect1[0]
+    x2_1, y2_1 = rect1[1]
+    x1_2, y1_2 = rect2[0]
+    x2_2, y2_2 = rect2[1]
+    
+    if x2_1 <= x1_2 or x2_2 <= x1_1:
+        return 0.0
+    
+    if y2_1 <= y1_2 or y2_2 <= y1_1:
+        return 0.0
+    
+    x_left = max(x1_1, x1_2)
+    x_right = min(x2_1, x2_2)
+    y_bottom = max(y1_1, y1_2)
+    y_top = min(y2_1, y2_2)
+    
+    width = x_right - x_left
+    height = y_top - y_bottom
+    
+    return float(width * height)
